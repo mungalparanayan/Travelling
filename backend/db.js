c4b11c8ff700f2dee158d09b8e658b71a1d23f25
mungalparanayan <nayanmungalpara3245@gmail.com>
2024-07-21 23:32:32 +0530

    changed mongoDB atlas

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://0.0.0.0/travel"
+// const mongoURI = "mongodb://0.0.0.0/travel"
+const mongoURI = "mongodb+srv://Travelling:TRAVEL@cluster0.i9vthyp.mongodb.net/Travel?retryWrites=true&w=majority&appName=Cluster0"
 
 const connectToMongo = ()=> {
     mongoose.connect(mongoURI, {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
+        // useUnifiedTopology: true,
     })
     .then(() => {
         console.log('Connected to MongoDB Successfully');
